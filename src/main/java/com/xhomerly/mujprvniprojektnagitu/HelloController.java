e2fedabe837229a4bf742d8fe806571dceb3545e
xhomerly <jan.lysy06@gmail.com>
2024-06-06 12:45:29 +0200

    Implementace zmeny barvy na auticku

--- src/main/java/com/xhomerly/mujprvniprojektnagitu/HelloController.java
+++ src/main/java/com/xhomerly/mujprvniprojektnagitu/HelloController.java
@@ -1,14 +1,14 @@
 package com.xhomerly.mujprvniprojektnagitu;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.shape.Polygon;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    @FXML private Polygon auto;
+    @FXML private ColorPicker colorPicker;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void changeColor() {
+        auto.setFill(colorPicker.getValue());
     }
 }
