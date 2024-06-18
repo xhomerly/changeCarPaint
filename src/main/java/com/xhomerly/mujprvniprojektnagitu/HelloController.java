038d17bed9c74e22bd911c6531cc58ae64b20663
xhomerly <jan.lysy06@gmail.com>
2024-06-18 09:49:37 +0200

    Fungujici nastaveni velikosti kol

--- src/main/java/com/xhomerly/mujprvniprojektnagitu/HelloController.java
+++ src/main/java/com/xhomerly/mujprvniprojektnagitu/HelloController.java
@@ -2,13 +2,26 @@ package com.xhomerly.mujprvniprojektnagitu;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 
 public class HelloController {
     @FXML private Polygon auto;
     @FXML private ColorPicker colorPicker;
+    @FXML private Slider slider;
+    @FXML private Circle frontWheel;
+    @FXML private Circle backWheel;
 
     private boolean wracked = false;
+
+    @FXML
+    public void initialize() {
+        slider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            frontWheel.setRadius(slider.getValue());
+            backWheel.setRadius(slider.getValue());
+        });
+    }
 
     public void changeColor() {
         auto.setFill(colorPicker.getValue());
